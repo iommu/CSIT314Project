6a4aaac39a188ef5ae84ffdff54adbd52dd1d055
samdahman <sg228@uowmail.edu.au>
2021-05-31 13:32:52 +1000

    fixed generate func

--- wolfTest/generate.py
+++ wolfTest/generate.py
@@ -142,4 +142,4 @@ def convert_units_gen(number_units, units=rand_units()):
     return f"convert {number_units} {random.choice(units)} to {random.choice(units)}"
 
 def volume_food_gen(volume, food_unit):
-    return ("calories in " + str(volume) + food_unit)
+    return "calories in  {} {}".format(volume,food_unit)
